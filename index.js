e8dfd28610aaaa72bb90ae04d3b1a2aa72161842
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-15 12:38:01 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Your code here
 const dodger=document.getElementById("dodger")
-    function moveDodgerLeft(){
-        const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
+if (left > 0) {
+dodger.style.left = `${left - 1}px`;
+}
 }
 function moveDodgerRight(){
-    const leftNumbers = dodger.style.left.replace("px", "");
+const leftNumbers = dodger.style.left.replace("px", "");
 const left = parseInt(leftNumbers, 10);
 
 if (left > 0) {
